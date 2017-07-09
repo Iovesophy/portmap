7239c5b6a16980eeeae434ad424c6c8ec0defde7
odennyuda <kazu825@me.com>
2017-07-10 01:12:11 +0900

    gitignore set

--- .auto_ssh_x.sh
+++
@@ -1,3 +0,0 @@
-#!bin/zsh
-#octagon pc port forwording
-ssh aws -x -R 10088:10.1.11.7:2222 sh .connect.sh
--- .autossh.rb
+++
@@ -1,7 +0,0 @@
-init = 100
-init.times do |i|
-	puts i
-	system("ssh aws -R 10022:10.1.11.7:2222 sh .connect.sh")
-	system("sleep 3")
-        i += 1
-end
